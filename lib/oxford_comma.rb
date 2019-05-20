a8bc4f77c6aaf6ac144f9ec5c474ff80bc264371
Sam Markham <sam.e.markham@gmail.com>
2019-05-20 18:50:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length > 2
     #the oxford comma stuff  
   elsif array.length = 2 
+    array.join(" and ")
     #convert into string and put an and between them 
   else 
     #do nothing 
