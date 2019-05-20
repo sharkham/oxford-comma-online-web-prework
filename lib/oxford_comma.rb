e32cbf71c22682a6bb8c852faa606f5a922b7bf6
Sam Markham <sam.e.markham@gmail.com>
2019-05-20 19:17:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    array[-1] = "and #{array[-1]}"
+    string = array.join(", ")
+  elsif array.length == 2 
+    array.join(" and ")
+    #convert into string and put an and between them 
+  else 
+    array.join
+    #do nothing 
+  end 
 end
