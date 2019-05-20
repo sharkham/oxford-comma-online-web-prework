0db27e0abbab8fdc69834d2e697fa56329a90977
Sam Markham <sam.e.markham@gmail.com>
2019-05-20 19:16:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   if array.length > 2
-    array.insert(-2, "and")
+    #array.insert(-2, "and")
+    array[-1] = "and #{array[-1]}"
     string = array.join(", ")
     #join with " " for element -1
     #NOPE wait turn the last element into "and + #{array[-1]}"
