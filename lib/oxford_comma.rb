1473d1de763ce092a5fa258c3a130b51c0784c39
Sam Markham <sam.e.markham@gmail.com>
2019-05-20 19:11:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.length > 2
     array.insert(-2, "and")
     string = array.join(", ")
-    #the oxford comma stuff  
+    #join with " " for element -1
+    #NOPE wait turn the last element into "and + #{array[-1]}"
   elsif array.length == 2 
     array.join(" and ")
     #convert into string and put an and between them 
